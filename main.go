5cc6333c7af9abbab3b4fed05ae3bdac64350202
AndrewSich <andrewsetyawan123@gmail.com>
2021-05-05 23:40:21 +0700

    second commit

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/gin-gonic/gin"
+	// "github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
@@ -16,14 +16,28 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+	//mux routing
+	http.HandleFunc("/", handlerIndex)
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
+	// run server
+	log.Printf("Server Running On\n")
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Prinln(err.Error())
+	}
 
-	router.Run(":" + port)
+	// router := gin.New()
+	// router.Use(gin.Logger())
+	// router.LoadHTMLGlob("templates/*.tmpl.html")
+	// router.Static("/static", "static")
+
+	// router.GET("/", func(c *gin.Context) {
+	// 	c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	// })
+
+	// router.Run(":" + port)
+}
+
+func handlerIndex(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome to Levxa Web Server"))
 }
