c883466a49d9e7c934d461ca3482c9c619f3e089
AndrewSich <andrewsetyawan123@gmail.com>
2021-05-06 00:12:09 +0700

    clear error

--- main.go
+++ main.go
@@ -4,10 +4,32 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	// "github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
+
+type Mux struct {
+	blog, main *http.ServeMux
+}
+
+func (mux Mux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println("[+] success ", r.Host)
+	if r.Host == "levxa-web-server.herokuapp.com" {
+		mux.main.ServeHTTP(w, r)
+		return
+	}
+
+	// subdomain checking
+	domainParts := strings.Split(r.Host, ".")
+	if domainParts[0] == "blog" {
+		mux.blog.ServeHTTP(w, r)
+	} else {
+		log.Printf("[!] warning Subdomain is not found!")
+		http.Error(w, "Pages Not Found", 404)
+	}
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -17,11 +39,18 @@ func main() {
 	}
 
 	//mux routing
-	http.HandleFunc("/", handlerIndex)
+	mux := &Mux{
+		blog: http.NewServeMux(),
+		main: http.NewServeMux(),
+	}
+
+	mux.blog.HandleFunc("/", handlerBlog)
+	mux.main.HandleFunc("/", handlerIndex)
 
 	// run server
+
 	log.Printf("Server Running On\n")
-	err := http.ListenAndServe(":"+port, nil)
+	err := http.ListenAndServe(":"+port, mux)
 	if err != nil {
 		log.Println(err.Error())
 	}
@@ -40,4 +69,9 @@ func main() {
 
 func handlerIndex(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Welcome to Levxa Web Server"))
+}
+
+func handlerBlog(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprintf(w, "This a Blog Pages", r.URL.Path[1:])
+	w.Write([]byte("This a Blog Pages"))
 }
